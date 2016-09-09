9edd837aaecc8858bbd79368c57abf336815f284
macos <macos@macoss-Air.nc.rr.com>
2016-09-08 22:40:49 -0400

    Added implementation for methods

--- src/main/java/FoldersStructure.java
+++ src/main/java/FoldersStructure.java
@@ -1,9 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 
 public class FoldersStructure {
 	String sourseFolderPath = null;
 	String targetFolderPath = null;
+	String lastSubFolderPath = null;
 
 	public void finalBuilder() {
 		// возвращет список папок в исходной папке
@@ -25,17 +35,21 @@ public class FoldersStructure {
 				// возваращет путь к файлу с именем файла
 				String fileNameSourceFullPath = listPhoto.next();
 				// возвращает информацию о файле, дату создания
-				String fileDate = getFileInfoDateCreation(fileNameSourceFullPath);
-				// в заданной папке,создает структуру папок основываясь на дате создания файла
+				HashMap<String, String> fileDate = getFileInfoDateCreation(fileNameSourceFullPath);
+				// в заданной папке,создает структуру папок основываясь на дате
+				// создания файла
 				String finalPath = folderStructurForFileCreation(targetFolderPath, fileDate);
-				// копирует файл в папку которая была созданана основании даты создания файла
+				// копирует файл в папку которая была созданана основании даты
+				// создания файла
 				String fileNameFullTargetPath = copyFileToTargetFolder(finalPath, fileNameSourceFullPath);
-				// сравнивает исходный файл и созданный, если равны удаляет оригинал
+				// сравнивает исходный файл и созданный, если равны удаляет
+				// оригинал
 				if (compareSourceAndTargetFile(fileNameSourceFullPath, fileNameFullTargetPath)) {
 					deleteSourceFile(fileNameSourceFullPath);
 				}
 			}
-			//после того как все файлы из папки скопированны и удалены, удаляет саму папку
+			// после того как все файлы из папки скопированны и удалены, удаляет
+			// саму папку
 			deleteEmptyFolder(lowFolderPath);
 		}
 	}
@@ -61,22 +75,97 @@ public class FoldersStructure {
 
 	}
 
-	private String folderStructurForFileCreation(String targetFolderPath, String fileDate) {
-		return null;
+	private String folderStructurForFileCreation(String targetFolderPath, HashMap<String, String> fileDate) {
+		// Creation Folder YEAR
+		File file = new File(targetFolderPath + fileDate.get("Year"));
+		if (file.mkdir()) {
+			System.out.println("Folder " + fileDate.get("Year") + " has been created.");
+		} else {
+			System.out.println("Folder " + fileDate.get("Year") + " was created previosly.");
+		}
+
+		// Creation Folder MONTH
+		file = new File(targetFolderPath + fileDate.get("Year") + "/" + fileDate.get("Month"));
+		if (file.mkdir()) {
+			System.out.println("Folder " + fileDate.get("Month") + " has been created.");
+		} else {
+			System.out.println("Folder " + fileDate.get("Month") + " was created previosly.");
+		}
+
+		// Creation Folder DAY
+		file = new File(
+				targetFolderPath + fileDate.get("Year") + "/" + fileDate.get("Month") + "/" + fileDate.get("Day"));
+		if (file.mkdir()) {
+			System.out.println("Folder " + fileDate.get("Day") + " has been created.");
+		} else {
+			System.out.println("Folder " + fileDate.get("Day") + " was created previosly.");
+		}
+		String finalFolderPath = targetFolderPath + fileDate.get("Year") + "/" + fileDate.get("Month") + "/"
+				+ fileDate.get("Day") + "/";
+
+		return finalFolderPath;
 
 	}
 
-	private String getFileInfoDateCreation(String object) {
-		return null;
+	private HashMap<String, String> getFileInfoDateCreation(String object) {
 
+		File file = new File(object);
+		HashMap<String, String> date = new HashMap<String, String>();
+		Metadata metadata = null;
+		try {
+			metadata = ImageMetadataReader.readMetadata(file);
+		} catch (ImageProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (Directory directory : metadata.getDirectories()) {
+			for (Tag tag : directory.getTags()) {
+				if (tag.getTagName().contains("Date/Time Original")) {
+					date.put("Year", ((tag.getDescription()).substring(0, 4)));
+					date.put("Month", ((tag.getDescription()).substring(5, 7)));
+					date.put("Day", ((tag.getDescription()).substring(8, 10)));
+				}
+			}
+			if (directory.hasErrors()) {
+				for (String error : directory.getErrors()) {
+					System.err.format("ERROR: %s", error);
+				}
+
+			}
+		}
+		return date;
 	}
 
 	private List<String> getListFilesAtFolder(String lowFolderPath) {
-		return null;
+		File directory = new File(lowFolderPath);
+		List<String> listFiles = new ArrayList<String>();
+		String[] files = directory.list();
+		for (int i = 0; i < files.length; i++) {
+			listFiles.add(lowFolderPath + "/" + files[i]);
+		}
+		return listFiles;
 	}
 
 	private String diveDownLowLevelFolderStructure(String sourseFolderPath) {
-		return null;
+		File file = new File(sourseFolderPath);
+		String[] names = file.list();
+		String folder = "folders is absent";
+
+		for (String name : names) {
+			if (new File(file.getPath() + "/" + name).isDirectory()) {
+				folder = file.getPath() + "/" + name;
+				break;
+			}
+		}
+
+		if (!folder.equals("folders is absent")) {
+			diveDownLowLevelFolderStructure(folder);
+		} else {
+			lastSubFolderPath = file.getPath();
+		}
+		return lastSubFolderPath;
 	}
 
 }
